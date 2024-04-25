7121cdcc3fa3315d28006825b43d9d5ad28d0877
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-04-25 00:46:37 -0500

    Add DepositConfig component into the CalculatorContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,13 @@
-function App () {
-  return(
-    <></>
+import { CalculatorContextProvider } from "./components/CalculatorContext";
+import DepositConfig from "./components/DepositConfig";
+
+function App() {
+  return (
+    <div>
+      <CalculatorContextProvider>
+        <DepositConfig />
+      </CalculatorContextProvider>
+    </div>
   )
 }
 
