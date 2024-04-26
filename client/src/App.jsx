3771ee65c4f00d9e364e74f4d5ee22e136090c8c
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-04-26 01:51:42 -0500

    Add main header tailwindcss styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import DepositDetail from "./components/DepositDetail";
 function App() {
   return (
     <div>
-      <header>
+      <header className="text-xl bg-sky-950 text-white text-center p-3 font-medium">
         <h1>Calculadora Depósito a Plazo Fijo</h1>
       </header>
       <CalculatorContextProvider>
