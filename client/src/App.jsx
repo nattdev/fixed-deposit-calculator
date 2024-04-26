b5eeeb578b723aefdc29cbe2a80f467b28beda38
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-04-25 20:38:00 -0500

    Add DepositDetail component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { CalculatorContextProvider } from "./components/CalculatorContext";
 import DepositConfig from "./components/DepositConfig";
+import DepositDetail from "./components/DepositDetail";
 
 function App() {
   return (
     <div>
       <CalculatorContextProvider>
         <DepositConfig />
+        <DepositDetail />
       </CalculatorContextProvider>
     </div>
   )
