c636abfecfa6e2708afe6334924c382491b1fde9
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-04-27 18:28:53 -0500

    Add header and wrapper section html elements

--- client/src/components/DepositDetail.jsx
+++ client/src/components/DepositDetail.jsx
@@ -6,25 +6,30 @@ function DepositDetail() {
 
     const currencySymbol = deposit["currency"] == "soles" ? "S/. " : "$ ";
     return (
-        <div>
-            <div>
-                <p>Con un Capital de <span>{deposit["capital"]} {currencySymbol == "S/. " ? "soles" : "dólares"}</span></p>
-                <p>por un Plazo de <span>{deposit["term"]} días</span></p>
-                <p>con un TREA de <span>{deposit["trea"]}  %</span></p>
-            </div>
+        <section>
+            <header>
+                <p>Detalle de plazo fijo</p>
+            </header>
             <div>
                 <div>
-                    <img src="#"></img>
-                    <p>El interés <span>Mensual</span> ganado es:</p>
-                    <p>{currencySymbol} {interestPeriodEarned}</p>
+                    <p>Con un Capital de <span>{deposit["capital"]} {currencySymbol == "S/. " ? "soles" : "dólares"}</span></p>
+                    <p>por un Plazo de <span>{deposit["term"]} días</span></p>
+                    <p>con un TREA de <span>{deposit["trea"]}  %</span></p>
                 </div>
                 <div>
-                    <img src="#"></img>
-                    <p><span>Al finalizar, </span>el total de fondos será:</p>
-                    <p>{currencySymbol} {interestEarned}</p>
+                    <div>
+                        <img src="#"></img>
+                        <p>El interés <span>Mensual</span> ganado es:</p>
+                        <p>{currencySymbol} {interestPeriodEarned}</p>
+                    </div>
+                    <div>
+                        <img src="#"></img>
+                        <p><span>Al finalizar, </span>el total de fondos será:</p>
+                        <p>{currencySymbol} {interestEarned}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
