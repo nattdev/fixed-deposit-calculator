60c68f06166d43e83f8e97a3a6d8ad15b3980f22
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-04-28 02:32:46 -0500

    Change the margin between elements of the parameters section

--- client/src/components/DepositConfig.jsx
+++ client/src/components/DepositConfig.jsx
@@ -80,7 +80,7 @@ function DepositConfig() {
                     </label>
                 </div>
             </section>
-            <section id="parameters" className="flex my-3 sm:mx-3 mx-1">
+            <section id="parameters" className="flex my-3 sm:mx-3 mx-2">
                 <div className="flex items-center flex-col gap-2 w-2/6">
                     <img src={currencyIcon} className="min-h-[56px] min-w-[56px]"></img>
                     <p>Capital</p>
@@ -92,7 +92,7 @@ function DepositConfig() {
                 <div className="flex items-center flex-col gap-2 w-2/6">
                     <img src={treaIcon} className="min-h-[56px] min-w-[56px]"></img>
                     <p>TREA</p>
-                    <div className="flex items-center justify-center mx-3 relative font-medium">
+                    <div className="flex items-center justify-center sm:mx-3 mx-2 relative font-medium">
                         <span className="absolute left-0 pl-3 ">% </span>
                         <input type="number" step={0.01} onChange={handleTreaInput} defaultValue={0.00} className="w-full border-2 rounded-2xl sm:pl-9 pl-7 sm:text-xl text-base"></input>
                     </div>
