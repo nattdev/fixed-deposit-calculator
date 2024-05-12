25a65293a99111709242ed7f83892095edb0c450
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-11 21:03:23 -0500

    Create DepositInfoComponent

---
+++ client/src/components/DepositInfo.jsx
@@ -0,0 +1,12 @@
+function DepositInfo() {
+    return (
+        <div className="flex justify-center items-center flex-col text-center my-6 mx-auto sm:w-2/4 px-2 w-3/4">
+            <p className="text-lg font-semibold mb-2">¿Qué es un depósito a plazo fijo en Perú?</p>
+            <p className="sm:text-base text-sm">
+                Es un instrumento financiero ofrecido por entidades bancarias y financieras donde un cliente deposita una cantidad de dinero por un período de tiempo específico a cambio de recibir un interés fijo predeterminado.
+            </p>
+        </div>
+    )
+}
+
+export default DepositInfo;
