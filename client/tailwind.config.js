8b755694e28167a772b72a610c903666d5399f7b
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-19 20:29:47 -0500

    Delete animation configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,18 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        'border-animation': 'rotate-animation 7s linear forwards;',
-      },
-      keyframes: {
-        'rotate-animation': {
-          '0%' : {transform: 'rotate(0deg)'},
-          '100%': { transform: 'rotate(360deg)',
-            opacity: '0' },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
