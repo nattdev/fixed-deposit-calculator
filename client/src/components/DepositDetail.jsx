c509860e264c9582d7dd347a72b1e29d6fe4be4d
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 13:35:05 -0500

    Add TotalInterest Item to Deposit Detail Component

--- client/src/components/DepositDetail.jsx
+++ client/src/components/DepositDetail.jsx
@@ -1,6 +1,7 @@
 import { useCalculator } from "./CalculatorContext";
 import EndInterestIcon from "../assets/icons/endInterestIcon.jsx";
 import MonthlyInterestIcon from "../assets/icons/monthlyInterestIcon.jsx";
+import TotalInterestIcon from "../assets/icons/totalInterestIcon.jsx";
 
 function DepositDetail() {
     const { deposit } = useCalculator();
@@ -8,6 +9,8 @@ function DepositDetail() {
     const { isCalculated } = useCalculator();
 
     const currencySymbol = deposit["currency"] == "soles" ? "S/. " : "$ ";
+    const totalAmount = parseFloat(deposit["capital"]) + parseFloat(interestEarned);
+
     return (
         <section className="flex flex-col sm:text-lg text-base justify-center items-center my-6 font-normal w-fit m-auto relative px-6">
             <header className="font-medium mb-3 text-xl">
@@ -20,7 +23,7 @@ function DepositDetail() {
                         <p>por un Plazo de <span className="font-bold">{deposit["term"]} días</span></p>
                         <p>con un TREA de <span className="font-bold">{deposit["trea"]}  %</span></p>
                     </div>
-                    <div className="flex justify-center gap-6">
+                    <div className="flex justify-center gap-6 flex-wrap">
                         <div className="flex flex-col w-2/5 items-center text-center sm:max-w-none max-w-32">
                             <MonthlyInterestIcon styles={`w-[56px] h-[56px] m-1 ${isCalculated ? "fill-black" : "fill-gray-300"} transition-colors duration-700`} />
                             <p>El interés <span>Mensual</span> ganado es:</p>
@@ -28,8 +31,13 @@ function DepositDetail() {
                         </div>
                         <div className="flex flex-col w-2/5  items-center text-center sm:max-w-none max-w-32">
                             <EndInterestIcon styles={`w-[56px] h-[56px] m-1 ${isCalculated ? "fill-black" : "fill-gray-300"} transition-colors duration-700`} />
-                            <p><span>Al finalizar</span>, el interés Total es:</p>
+                            <p>El interés <span>Total</span> ganado es:</p>
                             <p className="font-bold">{currencySymbol} {interestEarned}</p>
+                        </div>
+                        <div className="flex flex-col w-2/5  items-center text-center sm:max-w-none max-w-32">
+                            <TotalInterestIcon styles={`w-[56px] h-[56px] m-1 ${isCalculated ? "fill-black" : "fill-gray-300"} transition-colors duration-700`} />
+                            <p><span>Al finalizar</span> el plazo, tendrás:</p>
+                            <p className="font-bold">{currencySymbol} {totalAmount}</p>
                         </div>
                     </div>
                 </div>
