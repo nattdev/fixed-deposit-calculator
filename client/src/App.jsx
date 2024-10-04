0947616545f85fb1c99984adf7f6c1969065b99d
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 11:59:35 -0500

    Move DepositInfo Component to the bottom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,11 +10,11 @@ function App() {
       <header className="sm:text-xl text-lg bg-sky-950 text-white text-center p-3 font-medium">
         <h1>Calculadora Depósito a Plazo Fijo</h1>
       </header>
-      <DepositInfo />
       <CalculatorContextProvider>
         <DepositConfig />
         <DepositDetail />
       </CalculatorContextProvider>
+      <DepositInfo />
       <Footer />
     </div>
   )
